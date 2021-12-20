0552eec98d6e709d5f1d1b941c9dcf0f4ba8851d
dronkillerShark <88827209+dronkillerShark@users.noreply.github.com>
2021-12-20 14:03:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.sendFile('/Users/jameelgmail.com/Downloads/projects/project6/index.html');
 })
 
+app.get('*', (req, res) => {
+  res.send("you breaked the internet");
+})
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 });
